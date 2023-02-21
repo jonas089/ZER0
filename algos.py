e198fd9b582033198b9b292f4db224caa5af9673
jonas089 <jonas089@gmail.com>
2023-02-21 20:07:58 +0100

    produces json artifacts of entry_points, runtime args and return values

--- algos.py
+++ algos.py
@@ -5,21 +5,43 @@ def revert(error):
     print('Error code: ', error)
     return False
 
-def parse_entry_points(string):
+def parse_entry_points(c_string):
     entry_points = []
-    print('parsing entry points')
-    oA = re.finditer('ENTRY_POINT', string)
-    oE = re.finditer('END', string)
+    oA = re.finditer('ENTRY_POINT', c_string)
+    oE = re.finditer('END', c_string)
     oA_list = [occ for occ in oA]
     oE_list = [occ for occ in oE]
     if len(oA_list) != len(oE_list):
         revert(ERROR_LENGTH_MISMATCH)
-    print(oA_list)
-    print(oE_list)
     for i in range(0, len(oA_list)):
         _range = (oA_list[i].span(), oE_list[i].span())
-        entry_points.append(string[int(_range[0][1])+1:int(_range[1][0])-1])
+        entry_points.append(c_string[int(_range[0][1])+1:int(_range[1][0])-1])
     return entry_points
 
-def parse_entry_point_args(e_string):
-    pass
+def parse_entry_points_args(entry_points):
+    meta = {
+        'entry_points': [],
+        'vec!': [],
+        'ret': []
+    }
+
+    for i in range(0, len(entry_points)):
+        nAE = re.finditer('NAME', entry_points[i])
+        nAE_list = [ncc for ncc in nAE]
+        _range = (nAE_list[0].span(), nAE_list[1].span())
+        _name = entry_points[i][int(_range[0][1])+1:int(_range[1][0])-1]
+        meta['entry_points'].append(_name)
+
+        vAE = re.finditer('VEC', entry_points[i])
+        vAE_list = [vcc for vcc in vAE]
+        _range = (vAE_list[0].span(), vAE_list[1].span())
+        _vec = entry_points[i][int(_range[0][1])+1:int(_range[1][0])-1]
+        meta['vec!'].append(_vec)
+
+        rAE = re.finditer('RET', entry_points[i])
+        rAE_list = [rcc for rcc in rAE]
+        _range = (rAE_list[0].span(), rAE_list[1].span())
+        _res = entry_points[i][int(_range[0][1])+1:int(_range[1][0])-1]
+        meta['ret'].append(_res)
+
+    return meta
--- loader.py
+++ loader.py
@@ -1,17 +1,34 @@
-from algos import parse_entry_points, parse_entry_point_args
+import json, os
+from algos import parse_entry_points, parse_entry_points_args
+
 class Contract:
     def __init__(self, path):
         self.path = path
+        self.meta = None
     def read(self):
         with open(self.path, 'r') as file:
             return file.read()
     def entry_points(self):
         return parse_entry_points(self.read())
-
+    def metadata(self):
+        e = self.entry_points()
+        e_list = []
+        for ep in e:
+            e_list.append(ep)
+        contract_metadata = parse_entry_points_args(e_list)
+        self.meta = contract_metadata
+        return contract_metadata
+    def dump_metadata(self):
+        if self.meta == None:
+            _meta = self.metadata()
+        if os.path.exists('./meta.json'):
+            os.remove('./meta.json')
+        open('./meta.json', 'x')
+        with open('./meta.json', 'w') as file:
+            file.write(str(_meta))
 #tests
 def tests():
     c = Contract('./main.rs')
-    print(c.read())
-    e = c.entry_points()
-    print(e[0])
+    c.dump_metadata()
+
 tests()
